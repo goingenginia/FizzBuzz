881627c76419961e873d09e382e76b7cc763208f
goingenginia <goingenginia-programing@yahoo.co.jp>
2020-05-15 21:59:32 +0900

    指摘分を修正しました

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const fizzbuzz = document.getElementsByTagName("input");
+const fizz = document.getElementById("fizzinteger");
+const buzz = document.getElementById("buzzinteger");
 const elbtn = document.getElementById("btn");
 
-elbtn.addEventListener("click", function() {
+elbtn.addEventListener("click", function () {
   // inputの値を数字型に変える
-  let fizz = Number(fizzbuzz[0].value);
-  let buzz = Number(fizzbuzz[1].value);
+  let fizznumber = Number(fizz.value);
+  let buzznumber = Number(buzz.value);
 
   // 出力結果があれば消す
   while (document.getElementsByTagName("p")[3]) {
     document.getElementsByTagName("p")[3].remove();
   }
   // 両方が数値以外は処理を終える
-  if (Number.isNaN(fizz) || Number.isNaN(buzz)) {
+  if (Number.isNaN(fizznumber) || Number.isNaN(buzznumber)) {
     return false;
   }
 
@@ -20,17 +21,19 @@ elbtn.addEventListener("click", function() {
     // P要素を作成
     const elp = document.createElement("p");
     // ID emptyにp要素を子要素として追加する
-    document.getElementById("empty").appendChild(elp);
+    const elempty = document.getElementById("empty").appendChild(elp);
     // fizzbuzz fizz buzz の３パターンで条件分岐する
-    if (i % fizz === 0 && i % buzz === 0) {
+    if (i % fizznumber === 0 && i % buzznumber === 0) {
       //p要素の中身に値を入れる
       elp.textContent = "fizzbuzz" + i;
-    } else if (i % fizz === 0) {
+    } else if (i % fizznumber === 0) {
       // p要素の中身に値を入れる
       elp.textContent = "fizz" + i;
-    } else if (i % buzz === 0) {
+    } else if (i % buzznumber === 0) {
       //p要素の中身に値を入れる
       elp.textContent = "buzz" + i;
+    } else {
+      document.getElementById("empty").removeChild(elempty);
     }
   }
 });
